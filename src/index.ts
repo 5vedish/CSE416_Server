f8b1993a5e332b4418e8bf9b688a6120dde72b1e
Zhixing Zhao <zhixing.zhao@stonybrook.edu>
2021-10-07 17:59:23 -0700

    Heroku Deploy

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,34 @@
-import express from 'express';
-import cors from 'cors';
-import { PrismaClient } from '.prisma/client';
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from ".prisma/client";
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const prisma  = new PrismaClient();
+const prisma = new PrismaClient();
 
-app.use(cors({
-    origin: 'https://cse416democlient.herokuapp.com',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://cse416democlient.herokuapp.com",
+    credentials: true,
+  })
+);
 
-app.get('/', (req,res) => res.send('Express + TypeScript Server'));
+app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 
-app.post('/test', async (req, res) => {
-    res.status(200).json({
-            prisma: await prisma.user.create({
-              data: {
-                email: 'penis@gmail.com',
-                name: 'kyle',
-                question_bank: {
-                  create: [],
-                },
-              },
-            }),
-          });
-})
+app.post("/test", async (req, res) => {
+  res.status(200).json({
+    prisma: await prisma.user.create({
+      data: {
+        email: "penis@gmail.com",
+        name: "kyle",
+        question_bank: {
+          create: [],
+        },
+      },
+    }),
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at ${PORT}`);

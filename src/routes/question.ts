47b32e60cb680759340a4be2ca7134ed46d550cc
Zhixing Zhao <zhixing.zhao@stonybrook.edu>
2021-10-21 20:04:19 -0700

    added crud apps

--- src/routes/question.ts
+++ src/routes/question.ts
@@ -1,32 +1,77 @@
-import express from 'express';
-import { db } from '../db';
+import express from "express";
+import { db } from "../db";
 
 const questionRouter = express.Router();
 
 questionRouter.post("/", async (req, res) => {
-    const { question, choices, correctChoice } = req.body;
-    const correctChoiceIndex = parseInt(correctChoice);
-    const result = await db.question.create({
-        data: {
-            question,
-            choices,
-            correctChoice: correctChoiceIndex
-        }
-    });
-    res.json({ id: result.id });
+  const { question, choices, correctChoice } = req.body;
+
+  const correctChoiceIndex = parseInt(correctChoice);
+
+  const result = await db.question.create({
+    data: {
+      question,
+      choices,
+      correctChoice: correctChoiceIndex,
+    },
+  });
+
+  res.json({ id: result.id });
 });
 
 questionRouter.get("/:id", async (req, res) => {
-    const numericId = parseInt(req.params.id);
+  const numericId = parseInt(req.params.id);
 
-    const questionToCheck = await db.question.findFirst({
-        where: { id: numericId }
+  const questionToCheck = await db.question.findFirst({
+    where: { id: numericId },
+  });
+
+  if (!questionToCheck) {
+    res.status(404);
+    return;
+  }
+
+  res.json(questionToCheck);
+});
+
+questionRouter.put("/:id", async (req, res) => {
+  const numericId = parseInt(req.params.id);
+
+  const { question, choices, correctChoice } = req.body;
+  const correctChoiceIndex = parseInt(correctChoice);
+
+  const result = await db.question
+    .update({
+      where: {
+        id: numericId,
+      },
+      data: {
+        question,
+        choices,
+        correctChoice: correctChoiceIndex,
+      },
+    })
+    .catch((e) => {
+      res.sendStatus(404);
+      return;
     });
-    if (!questionToCheck) {
-        res.status(404);
-        return;
-    }
-    res.json({ correctChoice: questionToCheck.correctChoice});
+
+  res.sendStatus(200);
+});
+
+questionRouter.delete("/:id", async (req, res) => {
+  const numericId = parseInt(req.params.id);
+
+  const questionToCheck = await db.question
+    .delete({
+      where: { id: numericId },
+    })
+    .catch((e) => {
+      res.sendStatus(404);
+      return;
+    });
+
+  res.sendStatus(200);
 });
 
 export { questionRouter };

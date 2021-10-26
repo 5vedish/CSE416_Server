28525e503021f12ac74ff1d789d8e90df1be7b1f
Timothy Goltser <timothy@goltser.me>
2021-10-26 19:42:37 -0400

    Linty linty

--- src/routes/question.ts
+++ src/routes/question.ts
@@ -1,74 +1,74 @@
-import express from "express";
-import { db } from "../db";
+import express from 'express'
+import { db } from '../db'
 
-const questionRouter = express.Router();
+const questionRouter = express.Router()
 
-questionRouter.post("/", async (req, res) => {
-  const { question, choices, correctChoice } = req.body;
+questionRouter.post('/', async (req, res) => {
+    const { question, choices, correctChoice } = req.body
 
-  const correctChoiceIndex = parseInt(correctChoice);
+    const correctChoiceIndex = parseInt(correctChoice)
 
-  const result = await db.question.create({
-    data: {
-      question,
-      choices,
-      correctChoice: correctChoiceIndex,
-    },
-  });
-
-  res.json({ id: result.id });
-});
-
-questionRouter.get("/:id", async (req, res) => {
-  const numericId = parseInt(req.params.id);
-
-  const questionToCheck = await db.question.findFirst({
-    where: { id: numericId },
-  });
-
-  if (!questionToCheck) {
-    res.status(404);
-    return;
-  }
-
-  res.json(questionToCheck);
-});
-
-questionRouter.put("/:id", async (req, res) => {
-  const numericId = parseInt(req.params.id);
-
-  const { question } = req.body;
-
-  await db.question
-    .update({
-      where: {
-        id: numericId,
-      },
-      data: {
-        question
-      },
+    const result = await db.question.create({
+        data: {
+            question,
+            choices,
+            correctChoice: correctChoiceIndex,
+        },
     })
-    .catch((e) => {
-      res.sendStatus(404);
-      return;
-    });
 
-  res.sendStatus(200);
-});
+    res.json({ id: result.id })
+})
 
-questionRouter.delete("/:id", async (req, res) => {
-  const numericId = parseInt(req.params.id);
+questionRouter.get('/:id', async (req, res) => {
+    const numericId = parseInt(req.params.id)
 
-  await db.question
-    .delete({
-      where: { id: numericId },
+    const questionToCheck = await db.question.findFirst({
+        where: { id: numericId },
     })
-    .catch((e) => {
-      res.sendStatus(404);
-      return;
-    });
 
-  res.sendStatus(200);
-});
+    if (!questionToCheck) {
+        res.status(404)
+        return
+    }
 
-export { questionRouter };
+    res.json(questionToCheck)
+})
+
+questionRouter.put('/:id', async (req, res) => {
+    const numericId = parseInt(req.params.id)
+
+    const { question } = req.body
+
+    await db.question
+        .update({
+            where: {
+                id: numericId,
+            },
+            data: {
+                question,
+            },
+        })
+        .catch((e) => {
+            res.sendStatus(404)
+            return
+        })
+
+    res.sendStatus(200)
+})
+
+questionRouter.delete('/:id', async (req, res) => {
+    const numericId = parseInt(req.params.id)
+
+    await db.question
+        .delete({
+            where: { id: numericId },
+        })
+        .catch((e) => {
+            res.sendStatus(404)
+            return
+        })
+
+    res.sendStatus(200)
+})
+
+export { questionRouter }

d75d3c14ef38402dd995d5e05e34731d06c99375
Robert <robus.i@hotmail.com>
2021-10-27 17:10:40 -0400

    auth utils

---
+++ src/utils/auth.ts
@@ -0,0 +1,17 @@
+import { hash, compare } from 'bcryptjs';
+
+import { add, isFuture } from 'date-fns';
+
+const hashPassword = async (password: string) => {
+    return await hash(password, 16);
+};
+
+const checkPassword = async (password: string, hash: string) => {
+    return await compare(password, hash);
+};
+
+const verifyResetRequest = (createdAt: Date) => {
+    return isFuture(add(createdAt, { days: 1 }));
+};
+
+export { hashPassword, checkPassword };

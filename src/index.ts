91e134ba3e0d14004237a525c0d929762e3fc1b1
Robert <robus.i@hotmail.com>
2021-10-26 23:43:03 -0400

    added User schema

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-import { questionRouter } from './routes/question'
+import { questionRouter } from './routes/question';
 
-const app = express()
-const port = process.env.PORT || 8080 // default port to listen
+const app = express();
+const port = process.env.PORT || 8080; // default port to listen
 
 const corsOptions = {
     // origin: process.env.CLIENT_ORIGIN,
     origin: '*',
     credentials: true,
-}
+};
 
-app.use(cors(corsOptions))
-app.options('*', cors)
+app.use(cors(corsOptions));
+app.options('*', cors);
 
-app.use(express.json())
-app.use('/questions', questionRouter)
+app.use(express.json());
+app.use('/questions', questionRouter);
 
 // start the Express server
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
-    console.log(`server listening on ${port}`)
-})
+    console.log(`server listening on ${port}`);
+});
--- src/routes/question.ts
+++ src/routes/question.ts
@@ -1,12 +1,12 @@
-import express from 'express'
-import { db } from '../db'
+import express from 'express';
+import { db } from '../db';
 
-const questionRouter = express.Router()
+const questionRouter = express.Router();
 
 questionRouter.post('/', async (req, res) => {
-    const { question, choices, correctChoice } = req.body
+    const { question, choices, correctChoice } = req.body;
 
-    const correctChoiceIndex = parseInt(correctChoice)
+    const correctChoiceIndex = parseInt(correctChoice);
 
     const result = await db.question.create({
         data: {
@@ -14,30 +14,30 @@ questionRouter.post('/', async (req, res) => {
             choices,
             correctChoice: correctChoiceIndex,
         },
-    })
+    });
 
-    res.json({ id: result.id })
-})
+    res.json({ id: result.id });
+});
 
 questionRouter.get('/:id', async (req, res) => {
-    const numericId = parseInt(req.params.id)
+    const numericId = parseInt(req.params.id);
 
     const questionToCheck = await db.question.findFirst({
         where: { id: numericId },
-    })
+    });
 
     if (!questionToCheck) {
-        res.status(404)
-        return
+        res.status(404);
+        return;
     }
 
-    res.json(questionToCheck)
-})
+    res.json(questionToCheck);
+});
 
 questionRouter.put('/:id', async (req, res) => {
-    const numericId = parseInt(req.params.id)
+    const numericId = parseInt(req.params.id);
 
-    const { question } = req.body
+    const { question } = req.body;
 
     await db.question
         .update({
@@ -49,26 +49,26 @@ questionRouter.put('/:id', async (req, res) => {
             },
         })
         .catch((e) => {
-            res.sendStatus(404)
-            return
-        })
+            res.sendStatus(404);
+            return;
+        });
 
-    res.sendStatus(200)
-})
+    res.sendStatus(200);
+});
 
 questionRouter.delete('/:id', async (req, res) => {
-    const numericId = parseInt(req.params.id)
+    const numericId = parseInt(req.params.id);
 
     await db.question
         .delete({
             where: { id: numericId },
         })
         .catch((e) => {
-            res.sendStatus(404)
-            return
-        })
+            res.sendStatus(404);
+            return;
+        });
 
-    res.sendStatus(200)
-})
+    res.sendStatus(200);
+});
 
-export { questionRouter }
+export { questionRouter };

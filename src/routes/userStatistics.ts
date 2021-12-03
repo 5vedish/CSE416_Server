67337fcc18ba68d2d0646d0da23f16f736e0683a
Zhixing Zhao <zhixing.zhao@stonybrook.edu>
2021-12-03 00:05:01 -0800

    User Statistics Backend

--- src/routes/userStatistics.ts
+++ src/routes/userStatistics.ts
@@ -11,7 +11,7 @@ userStatsRouter.get('/', async (req, res) => {
 
     const { user } = req.session;
 
-    const aggregations = await db.quizAttempt.aggregate({
+    const attempt_aggregations = await db.quizAttempt.aggregate({
         _avg: {
             questionsCorrect: true,
         },
@@ -20,8 +20,46 @@ userStatsRouter.get('/', async (req, res) => {
         },
     });
 
+    const lifetimeQuizzes = await db.user.findFirst({
+        where: {
+            id: user.id,
+        },
+        include: {
+            _count: {
+                select: { quizAttempts: true },
+            },
+        },
+    });
+
+    const diff = await db.quizAttempt.groupBy({
+        by: ['difficulty'],
+        _count: true,
+    });
+
+    let diffs = { e: -1, m: -1, h: -1 };
+
+    diff.map((difficulty) => {
+        switch (difficulty.difficulty) {
+            case 'HARD':
+                diffs['h'] = difficulty._count;
+                break;
+            case 'MEDIUM':
+                diffs['m'] = difficulty._count;
+                break;
+            case 'EASY':
+                diffs['e'] = difficulty._count;
+                break;
+        }
+    });
+
+    console.log(diffs);
+
+    if (lifetimeQuizzes) console.log(lifetimeQuizzes._count);
+
     res.json({
-        averageScore: aggregations._avg ?? 0,
+        averageScore: attempt_aggregations._avg ?? 0,
+        lifetimeQuizzes: lifetimeQuizzes?._count,
+        diffs: diffs,
     });
 });
 
